3c7581dc6c4632c6479de82faa96c09605a29763
kenny_dizi <chitrung09t2@gmail.com>
2021-06-13 21:54:21 +0700

    update dist file

--- dist.js
+++ dist.js
@@ -2,13 +2,15 @@ const path = require('path');
 const fs = require('fs');
 
 const TREESTokenContract = path.resolve(__dirname, './build/contracts/TREESToken.json');
+const TREESTokenConfigContract = path.resolve(__dirname, './build/contracts/TREESTokenConfig.json');
+const BokkyPooBahsDateTimeLibraryContract = path.resolve(__dirname, './build/utils/BokkyPooBahsDateTimeLibrary.json');
 
 const getSubContent = ({abi, bytecode, sourceMap, source, compiler, schemaVersion}) => ({abi, bytecode, sourceMap, source, compiler, schemaVersion})
 
 const contractsContent = {
-  CharityChallenge: getSubContent(require(CharityChallengeContract)),
-  RealityCheckMock: getSubContent(require(RealityCheckMockContract)),
-  TestableCharityChallenge: getSubContent(require(TestableCharityChallengeContract))
+    TREESToken: getSubContent(require(TREESTokenContract)),
+    TREESTokenConfig: getSubContent(require(TREESTokenConfigContract)),
+    BokkyPooBahsDateTimeLibrary: getSubContent(require(BokkyPooBahsDateTimeLibraryContract))
 }
 
 fs.writeFileSync('./.exported.js', `module.exports = ${JSON.stringify(contractsContent)};`, 'utf-8');
