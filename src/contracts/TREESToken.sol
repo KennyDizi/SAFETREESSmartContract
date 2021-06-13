6963be9333c96aebad391bf58f8aa0488003bf5d
kenny_dizi <chitrung09t2@gmail.com>
2021-06-13 17:43:52 +0700

    use IERC165-supportsInterface from standard openzepplin

--- src/contracts/TREESToken.sol
+++ src/contracts/TREESToken.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.5;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "./TREESTokenConfig.sol";
-import "./interface/SupportsInterface.sol";
 
 // ----------------------------------------------------------------------------
 // TREESToken - TREES Token Contract
@@ -13,10 +12,7 @@ import "./interface/SupportsInterface.sol";
 //
 // ----------------------------------------------------------------------------
 
-contract TREESToken is ERC721, TREESTokenConfig, SupportsInterface {
+contract TREESToken is ERC721, TREESTokenConfig {
     constructor() ERC721(TOKEN_NAME, TOKEN_SYMBOL) {
-        supportedInterfaces[0x780e9d63] = true; // ERC721Enumerable
-        supportedInterfaces[0x5b5e139f] = true; // ERC721MetaData
-        supportedInterfaces[0x80ac58cd] = true; // ERC721
     }
 }
