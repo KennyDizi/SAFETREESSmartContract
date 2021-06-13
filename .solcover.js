54c32e3d3236dd68b81f937f00127840108da46d
kenny_dizi <chitrung09t2@gmail.com>
2021-06-13 21:36:23 +0700

    add truffle configuration

---
+++ .solcover.js
@@ -0,0 +1,9 @@
+module.exports = {
+  port: 8555,
+  testrpcOptions: '--port 8555 -m \"candy maple cake sugar pudding cream honey rich smooth crumble sweet treat\"',
+  skipFiles: [
+    'IRealityCheck.sol',
+    'RealityCheckMock.sol',
+    'Migrations.sol'
+  ]
+}
