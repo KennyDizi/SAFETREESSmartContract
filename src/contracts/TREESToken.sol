ec09eca4d74fe6b6fb51b32a5be1842eeda9d669
kenny_dizi <chitrung09t2@gmail.com>
2021-06-13 17:38:53 +0700

    add SupportsInterface

--- src/contracts/TREESToken.sol
+++ src/contracts/TREESToken.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.5;
+
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "./TREESTokenConfig.sol";
+import "./interface/SupportsInterface.sol";
 
 // ----------------------------------------------------------------------------
 // TREESToken - TREES Token Contract
@@ -11,7 +13,10 @@ import "./TREESTokenConfig.sol";
 //
 // ----------------------------------------------------------------------------
 
-contract TREESToken is ERC721, TREESTokenConfig {
+contract TREESToken is ERC721, TREESTokenConfig, SupportsInterface {
     constructor() ERC721(TOKEN_NAME, TOKEN_SYMBOL) {
+        supportedInterfaces[0x780e9d63] = true; // ERC721Enumerable
+        supportedInterfaces[0x5b5e139f] = true; // ERC721MetaData
+        supportedInterfaces[0x80ac58cd] = true; // ERC721
     }
 }
