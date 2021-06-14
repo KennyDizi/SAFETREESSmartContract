5c268bfe694dc481cd463ac2403b99301bc89f85
kenny_dizi <chitrung09t2@gmail.com>
2021-06-14 12:23:13 +0700

    add local deployer

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,1 +1,7 @@
-// TODO call smart contract and deployer
+// migrations/2_deploy_contracts.js, read more @ https://forum.openzeppelin.com/t/create-an-nft-and-deploy-to-a-public-testnet-using-truffle/2961
+// SPDX-License-Identifier: MIT
+const TREESToken = artifacts.require("TREESToken");
+
+module.exports = function(deployer) {
+  deployer.deploy(TREESToken, "SAFETREES Chain Token","TREES", "https://my-json-server/samplenft/tokens/");
+};
