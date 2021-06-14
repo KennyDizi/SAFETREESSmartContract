8357e0e6d0be4e53f699e101d8d7a8492ec64ab9
kenny_dizi <chitrung09t2@gmail.com>
2021-06-14 11:48:22 +0700

    update truffle configuration

--- truffle-config.js
+++ truffle-config.js
@@ -15,8 +15,30 @@
 module.exports = {
   compilers: {
     solc: {
-      version: "0.8.5"
+      version: "0.8.5",
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
     }
   },
-  plugins: ["solidity-coverage"]
+  plugins: ["solidity-coverage"],
+  // https://truffle.readthedocs.io/en/beta/advanced/configuration/
+  mocha: {
+    bail: true
+  },
+  /*
+  networks: {
+    // https://www.rinkeby.io/
+    // https://blog.abuiles.com/blog/2017/07/09/deploying-truffle-contracts-to-rinkeby/
+    rinkeby: {
+      host: "localhost",
+      port: 8945,
+      from: "0x7e5f938e0136F4e5deaA8D6Ea5760aeB9604cb1d", // enter your local rinkeby unlocked address
+      network_id: 4,
+      gas: 4500000, // Gas limit used for deploys
+      gasPrice: 10000000000 // 10 gwei
+    }
+  }
+  */
 };
